612d5902ace699967c0bd1355ee9351c46b7b54b
thisisnew <thisisnew@naver.com>
2021-01-28 18:26:24 +0900

    스트림 복습

---
+++ this-is-java/src/ch16/ParallelExample.java
@@ -0,0 +1,26 @@
+package ch16;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ParallelExample {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList(
+				"홍길동", "아무개", "김자바", "람다식", "박병렬"
+		);
+		
+		//순차
+		Stream<String> stream = list.stream();
+		stream.forEach(ParallelExample :: print);
+		System.out.println();
+		
+		//병렬
+		Stream<String> parallelStream = list.parallelStream();
+		parallelStream.forEach(ParallelExample :: print);
+	}
+	
+	public static void print(String str) {
+		System.out.println(str + " : " + Thread.currentThread().getName());
+	}
+}
